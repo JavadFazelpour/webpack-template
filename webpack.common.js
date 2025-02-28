be7204b257d6ee9bc4c78342e7af8618022c14f1
javad-fp <javad_fazelpour@outlook.com>
2025-02-28 19:42:02 +0330

    Set up project structure and add Webpack scripts
    - Create folder structure for assets, css, js, and dist.
    - Add Webpack configuration for development and production.
    - Include "start" and "build" scripts in package.json.
    - Add "clean" script to remove dist/ folder before builds.
    - Set up ESLint script for JavaScript linting (optional).
    
    These changes establish a solid project foundation with Webpack support
    and an organized folder structure.

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    assetModuleFilename: "images/[hash][ext][query]", // Organizes images in a separate folder
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource", // Handles images by outputting them as separate files
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,29 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "bundle.[contenthash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+});
